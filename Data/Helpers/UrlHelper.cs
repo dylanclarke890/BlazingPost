c5f3bd4a5db0170e967526345a48bfa36b3ff1ba
dylanclarke890 <dylanclarke890@gmail.com>
2021-11-08 20:45:38 +0000

    moved geturl to helper class and reorganised ui

---
+++ Data/Helpers/UrlHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlazingPostMan.Data.Helpers
+{
+    public static class UrlHelper
+    {
+        public static string GetUrl(string initialUrl, Dictionary<string, string> urlParams)
+        {
+            StringBuilder urlBuilder = new(initialUrl);
+            urlParams ??= new();
+
+            if (urlParams.Any())
+            {
+                urlBuilder.Append('?');
+                foreach (var keyValue in urlParams)
+                {
+                    urlBuilder.Append($"{keyValue.Key}={keyValue.Value}&");
+                }
+
+                // Remove trailing &
+                urlBuilder.Remove(urlBuilder.Length - 1, 1);
+            }
+
+            return urlBuilder.ToString();
+        }
+    }
+}
