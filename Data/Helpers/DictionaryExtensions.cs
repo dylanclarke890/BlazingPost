965efd4f2630a336e4cba4548c89cc94e6e3cf39
dylanclarke890 <dylanclarke890@gmail.com>
2021-11-14 20:54:11 +0000

    added error state provider, refactored postman initial vals to be passed oninitialized for better readability

--- Data/Helpers/DictionaryExtensions.cs
+++ Data/Helpers/DictionaryExtensions.cs
@@ -5,6 +5,7 @@ namespace BlazingPostMan.Data.Helpers
     public static class DictionaryExtensions
     {
         #nullable enable
+        /// <returns><c>False</c> if the key is null or adding failed, otherwise <c>True</c>.</returns>
         public static bool TryRemove(this Dictionary<string, string> keyValuePairs, string? key)
         {
             if (!string.IsNullOrEmpty(key))
