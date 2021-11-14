afb66087336c79a8e6d164fab6866b7ec506c103
dylanclarke890 <dylanclarke890@gmail.com>
2021-11-14 01:06:54 +0000

    added extension method to string,string dictionary for removing null values

---
+++ Data/Helpers/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BlazingPostMan.Data.Helpers
+{
+    public static class DictionaryExtensions
+    {
+        public static bool TryRemove(this Dictionary<string, string> keyValuePairs, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                return keyValuePairs.Remove(value);
+            }
+
+            return false;
+        }
+    }
+}
