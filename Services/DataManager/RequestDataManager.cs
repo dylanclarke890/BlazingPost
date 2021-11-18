47759149afd5bb8beb95e4f091c0a9272d9d825e
dylanclarke890 <dylanclarke890@gmail.com>
2021-11-18 19:11:39 +0000

    added key to keyvalues table for better rendering and started steps for requestdatamanager (to localise data)

---
+++ Services/DataManager/RequestDataManager.cs
@@ -0,0 +1,15 @@
+﻿using BlazingPostMan.Data.Enums;
+using BlazingPostMan.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPostMan.Services.DataManager
+{
+    public class RequestDataManager
+    {
+        public Dictionary<string, string> HeaderKeys { get; set; }
+        public Dictionary<string, string> QueryParamKeys { get; set; }
+    }
+}
